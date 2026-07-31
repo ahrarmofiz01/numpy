58b73d2676d631a74abd06b0c3652ea9c81b5286
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-07-31 21:00:10 +0530

    add code

--- num.py
+++ num.py
@@ -1,3 +1,19 @@
 import numpy as np
 arr=np.array((1,2,3,4,5))
 print(arr)
+a=np.array([1,2,3,4])
+print(a)
+#BASIC NYMPY
+arr=np.array(29)
+print(arr)#0D-array
+arr=np.array([1,2,3,4])
+print(arr)
+print(arr[0])
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+newarr = arr.reshape(4, 3)
+
+print(newarr)
+arr=np.array((1,"a",3.2,'b',5))
+print(arr)
+print(type(arr))
